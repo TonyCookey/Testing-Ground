a067bcc6e819eb268a3e09f281729969a39bd1a2
Tony Cookey <tonyiroegbu59@gmail.com>
2019-05-15 10:42:53 +0100

    changed some of the user functionalities and the button UI

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <h3>My APP</h3>
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
     <div class="col">
-      <router-link to="/" class="btn btn-primary mr-3">Home</router-link>
-      <router-link to="/hello" class="btn btn-success mr-3">Hello</router-link>
-      <router-link to="/user" class="btn btn-dark mr-3">User</router-link>
+      <router-link to="/" class="btn btn-outline-primary mr-3">Home</router-link>
+      <router-link to="/hello" class="btn btn-outline-success mr-3">Hello</router-link>
+      <router-link to="/user" class="btn btn-outline-dark mr-3">User</router-link>
       <router-view></router-view>
     </div>
   </div>
